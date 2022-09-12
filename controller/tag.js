360684fd3597bd7a6b0298ab038a57f63499fe23
ericc70 <eric070@net-c.fr>
2022-09-12 11:45:17 +0200

    add controller tag

--- controller/tag.js
+++ controller/tag.js
@@ -1,67 +1,102 @@
 const Tag = require('../model/tag')
 
-exports.CreateTag = (req, res, next) =>{
-    const tagObj = JSON.parse(req.body.tag)
-    delete tagObj._id;
-  
-    const tag = new Tag({
-      ...tagObj
-    });
-    tag.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
+exports.CreateTag = async (req, res) => {
+
+    try {
+        const tagObj = JSON.parse(req.body.tag)
+        delete tagObj._id;
+
+        const tag = new Tag({
+            ...tagObj
+        });
+        await tag.save()
+            .then(() => res.status(201).json({
+                message: 'Objet enregistré !'
+            }))
+
+    } catch (error) {
+        res.status(400).json({
+            error
+        })
+    }
+
 }
 
-exports.getAllTag = (req, res, next) =>{
-
-  Tag.find().then(
-    (tags) => {
-      res.status(200).json(tags);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-}
-exports.getOneTag = (req, res, next) =>{
-    Tag.findOne({
-        _id: req.params.id
-      }).then(
-        (tag) => {
-          res.status(200).json(tag);
-        }
-      ).catch(
-        (error) => {
-          res.status(404).json({
+exports.getAllTag = async (req, res) => {
+    try {
+        await Tag.find().then(
+            (tags) => {
+                res.status(200).json(tags);
+            }
+        )
+    } catch (error) {
+        res.status(400).json({
             error: error
-          });
-        }
-      );
+        });
+    }
 
 }
-exports.updateTag = (req, res, next) =>{
+
+exports.getOneTag = async (req, res) => {
+
+    try {
+        await Tag.findOne({
+            _id: req.params.id
+        }).then(
+            (tag) => {
+                res.status(200).json(tag);
+            }
+        )
+    } catch (error) {
+        res.status(404).json({
+            error: error
+        });
+    }
+
+}
+exports.updateTag = async (req, res) => {
 
     const tagObj = JSON.parse(req.body.tag)
-    
-    Tag.updateOne({_id : req.params.id}{...thingObject, _id: req.params.id})
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
+
+    try {
+        await Tag.updateOne({
+            _id: req.params.id
+        } {
+            ...thingObject,
+            _id: req.params.id
+        })
+            .then(() => res.status(201).json({
+                message: 'Objet enregistré !'
+            }))
+    } catch (error) {
+        res.status(400).json({
+            error
+        })
+    }
+
 
 }
 
 
 
-exports.deleteTag = (req, res, next) =>{
-    Tag.findOne({ _id: req.params.id })
-    .then(() => {
-      
-           Tag.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-          .catch(error => res.status(400).json({ error }));
-      
-    })
-    .catch(error => res.status(500).json({ error }));
+
+exports.deleteTag = async (req, res) => {
+
+    try {
+        await Tag.deleteOne({
+            _id: req.params.id
+        })
+            .then(() => res.status(200).json({
+                message: 'Objet supprimé !'
+            }))
+    } catch (error) {
+        res.status(500).json({
+            error
+        })
+    }
+
+
+
+
+
 }
